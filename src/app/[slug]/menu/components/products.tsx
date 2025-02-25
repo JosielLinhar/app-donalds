7e017072e0e2559044f6857171d90f033a1f37b4
JosielLinhar <iamlinhar@gmail.com>
2025-02-24 22:37:29 -0300

    feat: add product details page

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,7 +1,9 @@
-  import { Product } from "@prisma/client";
+import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+
+  import { formatCurrency } from "@/helpers/format-currency";
 
 interface ProductsProps {
   products: Product[];
@@ -16,10 +18,7 @@ const Products = ({ products }: ProductsProps) => {
         <div>
           <h3 className="text-sm font-medium">{product.name}</h3>
           <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
-          <p className="pt-3 text-sm font-semibold">{new Intl.NumberFormat("pt-BR", {
-            style: 'currency',
-            currency: 'BRL'
-          }).format(product.price)}</p>
+          <p className="pt-3 text-sm font-semibold">{formatCurrency(product.price)}</p>
         </div>
 
         {/* DIREITA */}
